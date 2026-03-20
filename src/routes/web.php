dbcc00e0e6778cad8b9a9f11075f70dafcce487d
yoshinobu k <ysnb5884@gmail.com>
2026-03-20 14:22:17 +0900

    feat: web.php ルート追加・コントローラー作成・タイムゾーン設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminAttendanceController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\AdminStaffController;
+use App\Http\Controllers\StampCorrectionRequestController
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/admin/login',[\App\Http\Controllers\AuthController::class,'showLoginForm']);
+Route::post('/admin/login',[\App\Http\Controllers\AuthController::class,'login']);
+Route::get('/admin/attendance/list', [AdminAttendanceController::class, 'index']);
+Route::get('/admin/attendance/{id}', [AdminAttendanceController::class, 'show']);
+Route::get('/admin/staff/list',[AdminStaffController::class,'index']);
+Route::get('/stamp_correction_request/list',[StampCorrectionRequestController::class,'index']);
+Route::post('/stamp_correction_request/approve/{attendance_correct_request_id}',[StampCorrectionRequestController::class,'create']);
+
