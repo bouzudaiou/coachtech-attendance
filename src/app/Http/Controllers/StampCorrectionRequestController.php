58599af3d5684cca22d7f519a1ecbfc8f415004c
yoshinobu k <ysnb5884@gmail.com>
2026-03-30 14:38:44 +0900

    feat: StampCorrectionRequestController index()実装
    - roleで分岐して申請一覧を返す
    - 管理者：全ユーザーの承認待ち・承認済み申請を取得（with user）
    - 一般ユーザー：自分の承認待ち・承認済み申請を取得

--- src/app/Http/Controllers/StampCorrectionRequestController.php
+++ src/app/Http/Controllers/StampCorrectionRequestController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\StampCorrectionRequest;
 use Illuminate\Http\Request;
 
 class StampCorrectionRequestController extends Controller
 {
-    //
+    public function index()
+    {
+        if (auth()->user()->role === 'admin') {
+            $pendingRequests = StampCorrectionRequest::where('status', '承認待ち')
+                ->with('user')
+                ->get();
+            $approvedRequests = StampCorrectionRequest::where('status', '承認済み')
+                ->with('user')
+                ->get();
+        } else {
+            $user = auth()->user();
+            $pendingRequests = StampCorrectionRequest::where('user_id', $user->id)
+                ->where('status', '承認待ち')
+                ->get();
+
+            $approvedRequests = StampCorrectionRequest::where('user_id', $user->id)
+                ->where('status', '承認済み')
+                ->get();
+        }
+        return view('stamp_correction_request.list', compact('pendingRequests', 'approvedRequests'));
+    }
+
+    public function show($attendance_correct_request_id) {
+
+
+    }
+
+
 }
